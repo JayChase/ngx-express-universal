75a03d89608143f81b29bfa53f9633f76e8b8c2b
JayChase <JonathanChase@outlook.com>
2017-08-11 15:11:57 +0900

    init base app ready to add universal

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { MdSidenavModule, MdButtonModule, MdToolbarModule, MdIconModule, MdListModule } from '@angular/material';
+
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Comp1Component,
+    Comp2Component
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdSidenavModule,
+    MdButtonModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdListModule,
+    RouterModule.forRoot([
+      {
+        path: 'comp1',
+        component: Comp1Component
+      },
+      {
+        path: 'comp2',
+        component: Comp2Component
+      },
+      {
+        path: '',
+        redirectTo: 'comp1',
+        pathMatch: 'full'
+      },
+      {
+        path: '**',
+        component: Comp1Component
+      }
+    ]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
