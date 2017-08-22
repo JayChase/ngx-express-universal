e2155068fb15232b2ef378e962da44a68bdb20e7
JayChase <JonathanChase@outlook.com>
2017-08-22 19:37:48 +0900

    path fixes

--- server/server.js
+++ server/server.js
@@ -1,19 +1,18 @@
 require('zone.js/dist/zone-node');
 const renderModuleFactory = require('@angular/platform-server').renderModuleFactory;
-const AppServerModuleNgFactory = require('../dist-server/main.d8532c2d5f13c00de031.bundle.js').AppServerModuleNgFactory;
-// const index = require('fs').readFileSync('./dist-server/index.html', 'utf8'); << for server side render
-const index = require('fs').readFileSync('./dist/index.html', 'utf8'); // for server transition
-const express = require('express');
+const AppServerModuleNgFactory = require('../dist-server/main.0f820128f95c4d7066e8.bundle.js').AppServerModuleNgFactory; // bundle name set by npm script build-set-server-main-bundle
 const path = require('path');
+const index = require('fs').readFileSync(path.join(__dirname, '..', '/dist-server/index.html'), 'utf8'); // for server side rendering
+// const index = require('fs').readFileSync(path.join(__dirname, '..', 'dist/index.html'), 'utf8'); // for server to client transition
+const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
-
-app.use(express.static(path.join(__dirname, '../dist'),{
+app.use(express.static(path.join(__dirname, '..', 'dist'), {
     index: false
 }));
 app.use('*', function (req, res, next) {
     // res.sendFile(path.join(__dirname,'../dist/index.html'));
-    renderModuleFactory(AppServerModuleNgFactory, { document: index, url: req.originalUrl }).then(html => {
+    renderModuleFactory(AppServerModuleNgFactory, { document: index, url: req.baseUrl }).then(html => {
         res.send(html);
     });
 });
